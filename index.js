ec1bd8b3ab538dd940e36dda107f663e81ef516a
geoffrey <geoffrey.waiganjo@student.moringaschool.com>
2024-07-03 14:54:41 +0300

    lab.md

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name : "Geoffrey",
+    streetAddress : "Sigona "
+}
+function updateEmployeeWithKeyAndValue(employee, streetAddress ,Sigona){
+    return {
+        ...employee,
+        [streetAddress] : "11 Broadway"
+    }
+   
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress,Sigona){
+   employee[streetAddress] = "12 Broadway"
+   return employee
+
+}
+
+function deleteFromEmployeeByKey(employee, name){
+    const newEmployee = {...employee }
+    delete newEmployee[name]
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee[name]
+    return employee
+}
+
